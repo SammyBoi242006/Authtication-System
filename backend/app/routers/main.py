73ff75e26da95198c1c1f17347f5017b132e8cd2
Samarth Shah <samarthshah723@gmail.com>
2025-10-22 16:01:43 +0530

    Another change

--- backend/app/routers/main.py
+++ backend/app/routers/main.py
@@ -65,6 +65,6 @@ async def web_signin(
         ''')
 
 if __name__ == "__main__":
-    uvicorn.run("backend.app.routers.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("backend.app.routers.main:app",reload=True)
 
 handler = Mangum(app)
